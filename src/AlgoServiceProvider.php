4ae80d805a3bbb5d143716eb8dddcfd6da4019c9
francoism90 <francoism90@users.noreply.github.com>
2024-10-30 14:51:05 +0100

    wip

--- src/AlgoServiceProvider.php
+++ src/AlgoServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Foxws\Algos;
 
-use Foxws\LaravelAlgos\Commands\LaravelAlgosCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -10,16 +9,8 @@ class AlgoServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-algos')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel_algos_table')
-            ->hasCommand(LaravelAlgosCommand::class);
+            ->hasConfigFile();
     }
 }
