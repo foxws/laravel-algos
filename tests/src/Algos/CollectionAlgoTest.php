103130bdd70623713e49dda26081a94e1e166300
francoism90 <francoism90@users.noreply.github.com>
2024-10-30 15:54:36 +0000

    Fix styling

--- tests/src/Algos/CollectionAlgoTest.php
+++ tests/src/Algos/CollectionAlgoTest.php
@@ -4,9 +4,6 @@ use Foxws\Algos\Enums\Status;
 use Foxws\Algos\Tests\TestCase;
 use Foxws\Algos\Tests\TestClasses\FakeCollectionAlgo;
 
-use function PHPUnit\Framework\assertEquals;
-use function PHPUnit\Framework\assertFalse;
-
 uses(TestCase::class);
 
 it('will determine that algo is success', function () {
