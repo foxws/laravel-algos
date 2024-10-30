f9bcc80ac3feed861e68c4cb9b0be82218ee90d9
francoism90 <francoism90@users.noreply.github.com>
2024-10-30 20:53:04 +0100

    wip

--- tests/src/Models/User.php
+++ tests/src/Models/User.php
@@ -3,7 +3,6 @@
 namespace Foxws\Algos\Tests\Models;
 
 use Foxws\Algos\Tests\Database\Factories\UserFactory;
-use Foxws\ModelCache\Concerns\InteractsWithModelCache;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -12,7 +11,6 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory;
-    use InteractsWithModelCache;
     use Notifiable;
 
     protected $guarded = [];
